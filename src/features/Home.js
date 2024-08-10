0d8b8d78e2d5953ab46655f9d6b2b7db2cd28075
CoreCD3 <talae1111@hotmail.com>
2024-08-10 15:26:02 +0700

    lab1-6

---
+++ src/features/Home.js
@@ -0,0 +1,29 @@
+import React, { useState } from 'react';
+import AddForm from './Product/AddForm';
+import Product from './Product/index';
+import data from './data.js';
+
+let currentProductId = 9;
+
+const Home = () => {
+  const [products, setProducts] = useState(data);
+
+  const addProduct = (product) => {
+    const newProduct = { id: ++currentProductId, ...product };
+    setProducts([...products, newProduct]);
+  };
+
+  return (
+    <div> 
+      <h1>New Products</h1>
+      <ul className="Home__products">
+        {products.map((product) => (
+          <Product key={product.id} item={product} />
+        ))}
+      </ul>
+      <AddForm addProduct={addProduct} />
+    </div>
+  );
+};
+
+export default Home;
